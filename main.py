c59e179dcd45332103c23af93ac395e51d034a2d
Himanshu <294himangshu@gmail.com>
2026-06-12 11:11:42 +0530

    3004 port added

--- main.py
+++ main.py
@@ -103,6 +103,6 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host=os.getenv("APP_HOST", "0.0.0.0"),
-        port=int(os.getenv("APP_PORT", "3008")),
+        port=int(os.getenv("APP_PORT", "3004")),
         reload=True,
     )
